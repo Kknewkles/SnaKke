8d8e191e10d8b4cc52b5eb0a6d8108399aaafe7e
Kknewkles <Kknewkles@gmail.com>
2015-09-25 14:03:38 +0300

    19 - semblance of proper workage; motion and spin - ok. KENNEDY, WE'RE FINALLY IN THE GODDAMN 3D SPACE.
    To do:
    - correct input sequence
    - correct initial tail moveTo
    - proper time-based rotation calculation

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -11,20 +11,25 @@ public class SnakeController : MonoBehaviour
     public GameObject SnakeTail;                        // le tails.
     List<GameObject> Snake = new List<GameObject>();    // le snake.
 
+    private FruitController Fruit;
+    bool spawnTail = false;
+    int spawnCounter = 0;
+
     GameObject inputManagerObject;
     InputManager inputManager;
     int[] controls = new int[2];
     
-    Vector3[] moveTo = new Vector3[10];     // array of next coords for tails; 10 max atm.
-    /*
-    Quaternion rotateTo = Quaternion.Euler(0, 0, 0);    // not sure about this one.
-    Vector3 angle = new Vector3(0, 0, 0);   // this might be better. Try constructing it yourself and then applying to the head.
-    */
-
+    public Vector3[] moveTo = new Vector3[10];     // array of next coords for tails; 10 max atm.
+    
+    // durations --- 
     [SerializeField] float initialDelay = 1f; // delay after launch    
     [SerializeField] float rotationDelay = 1.5f; // delay between cycles.
     
     public float rotationSpeed = 20;    // change this into duration later
+    
+    public float movDuration;
+    float movElapsed = 0;
+    float movAccuracy = 0.01f;
 
 
     Vector3 forV;   // vector for the head to form next coords for the head?
@@ -34,6 +39,7 @@ public class SnakeController : MonoBehaviour
 
     Vector3 horAxis = new Vector3(0, 1, 0);     // right of a classic basis
     Vector3 verAxis = new Vector3(1, 0, 0);     // up of a classic basis
+    //Vector3[] axes = { new Vector3(0, 1, 0), new Vector3(-1, 0, 0) };
 
     int[] nullArray = { 0, 0 };
     public int[] controlCheck = { 0, 0 };
@@ -46,6 +52,10 @@ public class SnakeController : MonoBehaviour
         SnakeHead = GameObject.FindWithTag("SnakeHead");
         Snake.Add(SnakeHead);
 
+        // get the fruit!
+        GameObject fruitControllerObject = GameObject.FindWithTag("Fruit");
+        Fruit = fruitControllerObject.GetComponent<FruitController>();
+        
         // get the input! ---
         inputManagerObject = GameObject.FindWithTag("InputManager");
         inputManager = inputManagerObject.GetComponent<InputManager>();
@@ -54,12 +64,37 @@ public class SnakeController : MonoBehaviour
         SnakeHead.transform.position = new Vector3(5, 0, 5);        
 
         // initial movement - go _forward_.
-        moveTo[0] = Snake[0].transform.position + Snake[0].transform.forward;
+        //moveTo[0] = Snake[0].transform.position + Snake[0].transform.forward;
                 
         // launch ---
         StartCoroutine(SnakeCycle());
     }
-    
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            return;
+        }
+        inputManager.Check(controlCheck);
+    }
+
+    // This is... eh. Probably ok.
+    void ProcessInput(int[] controls)
+    {
+        // hor axis
+        if (controls[0] != 0)
+        {
+            StartCoroutine(SnakeRotate(controls[0] * horAxis));
+        }
+        // ver axis
+        else if (controls[1] != 0)
+        {
+            StartCoroutine(SnakeRotate(controls[1] * -verAxis));
+        }
+    }
+
+
     // hehe, we might not even need the Update().
     IEnumerator SnakeCycle()
     {
@@ -67,66 +102,43 @@ public class SnakeController : MonoBehaviour
         while (alive)   // snake lives, snake crawls.
                         // TO DO: think about introducing a pause bool. if not for pause, at least for options.
         {
-            // assign values from input; or assign final values.
-
-
             // if there is a rotation, rotate
             if (controlCheck != nullArray)
             {
                 ProcessInput(controlCheck);
+                //StartCoroutine(SnakeRotate(controls[axis] * axisArray[axis]));
+
+                // reset input
                 controlCheck = new int[] {0,0};
             }
-
-            // move, always
             yield return new WaitForSeconds(rotationDelay);
 
+            // move, always
+            forV = Snake[0].transform.forward;
+            moveTo[0] = Snake[0].transform.position + forV;
 
+            StartCoroutine(SnakeCrawl());
+            yield return new WaitForSeconds(movDuration);
+
+            // spawn conditions
+            if (spawnTail && (spawnCounter-- == 0))
+            {
+                AddTail();
+                spawnTail = false;
+            }
         }
     }
-    
-    void Update()
-    {
-        if (isMoving)
-        {
-            return;
-        }
-
-        inputManager.Check(controlCheck);
-    }
-
-    // This is... eh. Probably ok.
-    // Zody has one func for each axis, you better do too.
-    void ProcessInput(int[] controls)
-    {
-        if (controls[0] != 0)
-        {
-            StartCoroutine(SnakeRotate(controls[0] * horAxis));
-        }
-        else if (controls[1] != 0)
-        {
-            StartCoroutine(SnakeRotate(controls[1] * -verAxis));
-        }
-    }
-
-    void AngleCorrection()
-    {
-        Vector3 angle = transform.rotation.eulerAngles;
-        angle.x = Mathf.Round(angle.x / 90) * 90;
-        angle.y = Mathf.Round(angle.y / 90) * 90;
-        angle.z = Mathf.Round(angle.z / 90) * 90;
-        transform.rotation = Quaternion.Euler(angle);
-    }
-    
-
+        
     IEnumerator SnakeRotate(Vector3 axis)
     {
         isMoving = true;    // false it when crawl ends
+                            // when you do correct input sequence.
 
-        // Time base it!
+        // Time base this!
         float currAngle = 0;
         while(currAngle < 90)
         {
-            float rotProgr = Time.deltaTime * rotationSpeed;  // leave it like this for now, but turn into time after.
+            float rotProgr = Time.deltaTime * rotationSpeed;
             if (rotProgr + currAngle > 90)
             {
                 rotProgr = 90 - currAngle;
@@ -142,9 +154,66 @@ public class SnakeController : MonoBehaviour
         isMoving = false;
     }
 
-    // stub
     IEnumerator SnakeCrawl()
     {
-        if(true) { yield return 0; }
-    }    
+        forV = Snake[0].transform.forward;
+        moveTo[0] = Snake[0].transform.position + forV;
+        
+        for (int i = 1; i < Snake.Count + 1; i++)
+        {
+            moveTo[i] = Snake[i-1].transform.position;
+        }
+        
+        movElapsed = 0;
+
+        while(movElapsed < movDuration)
+        {
+            for (int i = 0; i < Snake.Count; i++)
+            {
+                Vector3 start = Snake[i].transform.position;
+                Vector3 target = moveTo[i];
+                Vector3 range = target - start;
+
+                movElapsed = Mathf.MoveTowards(movElapsed, movDuration, Time.deltaTime);
+                Snake[i].transform.position = start + range * (movElapsed / movDuration);
+            }
+            yield return null;
+        }
+
+        for (int i = 0; i < Snake.Count; i++)
+        {
+            Snake[i].transform.position = moveTo[i];
+        }
+    }
+
+
+    void AngleCorrection()
+    {
+        Vector3 angle = transform.rotation.eulerAngles;
+        angle.x = Mathf.Round(angle.x / 90) * 90;
+        angle.y = Mathf.Round(angle.y / 90) * 90;
+        angle.z = Mathf.Round(angle.z / 90) * 90;
+        transform.rotation = Quaternion.Euler(angle);
+    }
+
+    // collider move time soon
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fruit"))
+        {
+            Fruit.OnEaten();
+            spawnTail = true;
+            spawnCounter = 1;
+        }
+    }
+
+    public void AddTail()
+    {
+        Vector3 move = Snake[0].transform.forward;
+        Vector3 coords = Snake[Snake.Count - 1].transform.position - move;
+
+        // if you take one of existing/from objectpool, how do you distinguish and refer between them?
+        SnakeTail = (GameObject)(Instantiate(SnakeTail, coords, Quaternion.identity));
+        Snake.Add(SnakeTail);
+    }
 }
