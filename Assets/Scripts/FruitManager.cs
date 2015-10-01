b48575d9450fe945a797239483c4b8efccf05c3e
Kknewkles <Kknewkles@gmail.com>
2015-10-01 17:16:34 +0300

    30 - Fruit fixes

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class FruitManager : MonoBehaviour
 {
-	// Spawn a fruit on game start, spawn another on fruit being eaten.
-    //private GameObject fruit;
+    public static FruitManager instance;
     
-    private int fruitCount = 0;
+    // Spawn a fruit on game start, spawn another on fruit being eaten.
+    public GameObject fruit;
+    
+    [HideInInspector] public int fruitsEaten = 0;
     // Snake should know how much fruits there are and generate enough snake tails.
     
+    void Awake()
+    {
+        instance = this;
+    }
+
     void Start()
     {
         Show();
@@ -17,36 +24,39 @@ public class FruitManager : MonoBehaviour
     // again, this is snake, so we don't need to check who's the one bumpin'.
     // But we need to refer to snake to grow it a tail.
     // Or, if I had a better idea about HOW TO EFFIN CODE...
-    void OnTriggerEnter(Collider other)
-    {
-        // Fruit's eaten, snake's growin'
-        OnEaten();
-
-        SnakeController.instance.spawnTail = true;
-        SnakeController.instance.spawnCounter = 1;
-    }
+    
 
 	void Show()
     {
         // Get level sizes from LevelManager
-        int rX, rY, rZ;
+        Vector3 fruitCoords = new Vector3(0, 0, 0);
         
-        rX = Random.Range(0, 10);
-        rY = Random.Range(0, 10);
-        rZ = Random.Range(0, 10);
-
-        // Check for snake coords before spawning the fruit;
-
-        // And how the hell do you check obstacles?...
-        // have a list of obstacles, I guess
-        // Ooh! Read from XML! Or list.
+        // pull out to separate function
+        
 
         // reroll if snake occupies (rX, rY, rZ)
 
+        // reroll is obstacle occupies
+
         // add a scale up animation for popping up?
-        transform.position = new Vector3(rX, rY, rZ);
+        /*while(!ok)
+            fruitCoords = FruitCoordinates();
+        transform.positon = fruitCoords;*/
+
+        // Spawn should be here?
     }
 
+    Vector3 FruitCoordinates()
+    {
+        int rX, rY, rZ;
+        
+        rX = Random.Range(0, XMLToLevel.instance.levelLength_x);
+        rY = Random.Range(0, XMLToLevel.instance.levelLength_y);
+        rZ = Random.Range(0, XMLToLevel.instance.levelLength_z);
+
+        return new Vector3(rX, rY, rZ);
+    }
+    
     // This might die out completely once I hook everything up to the ObjectPool.
     // void Recycle()
     void Hide()
@@ -57,7 +67,7 @@ public class FruitManager : MonoBehaviour
 
     public void OnEaten()
     {
-        fruitCount++;
+        
         Hide();
         Show();
     }
---
+++ Assets/Scripts/Fruit_OnEaten.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit_OnEaten : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+            // play sound
+        FruitManager.instance.fruitsEaten++;
+
+        SnakeController.instance.spawnTail = true;
+        SnakeController.instance.spawnCounter = 1;
+
+        FruitManager.instance.fruit.Recycle();
+    }
+    
+  
+}
